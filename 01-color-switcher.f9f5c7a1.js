a77db100f0a77d95dbcdeb875c1750fa54039f34
olhaizbash <127290975+olhaizbash@users.noreply.github.com>
2023-08-06 16:30:02 +0000

    Deploying to gh-pages from @ olhaizbash/goit-js-hw-09@2f89a9f54a41b79afdaf9ead692e256d7eb6cf65 🚀

---
+++ 01-color-switcher.f9f5c7a1.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.querySelector("body"),n=null;t.addEventListener("click",(function(d){n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(a){clearInterval(n),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.f9f5c7a1.js.map
